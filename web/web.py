16083c5e261a374fdefe66d8e1c3ab5a13ccabaf
coolfrancy <154463480+coolfrancy@users.noreply.github.com>
2025-03-08 00:52:46 +0000

    tying to save file

--- web/web.py
+++ web/web.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request
+import os
+
 
 app=Flask(__name__)
 
+#home page allows you to add files
 @app.route('/')
 def home():
     return render_template('home.html')
 
+#shows chart
 @app.route('/chart', methods=['GET', 'POST'])
 def chart_view():
-    if request.method=="POST":
-        chart_files=request.files['cfile']
-        return f'got file {chart_files.filename}'
-    else:
-        return 'wtf is going on'
+    if 'cfile' not in request.files:
+        return 'No file was given'
+    
+    file=request.files['cfile']
+
+    if file.filename=='':
+        return ' No selected file'
+    os.makedir('/workspaces/TosChartWeb/web', exit_ok=True)
+    file_path=os.path.join('/workspaces/TosChartWeb/web', file.filename)
+    file.save(file_path)
+    return 'file saved'
+    
+
 if __name__=='__main__':
     app.run(debug=True, port=5001)
